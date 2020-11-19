00950cd961447f68bb21346c21c9f36e69cfbf63
Pukka1 <ktackie@id.iit.edu>
2020-11-19 13:16:14 -0500

    created game play background

---
+++ assets/js/game.js
@@ -0,0 +1,13 @@
+
+window.onload=function () {
+var game = new Phaser.Game();
+}
+
+var config ={
+    width: 800,
+    height: 600,
+    backgroundColor: 0*000000,
+    scene:[Scene1, Scene2]
+}
+
+var game = new Phaser.Game(config);
---
+++ assets/js/scene1.js
@@ -0,0 +1,13 @@
+class Scene1 extends Phaser.Scene {
+  constructor() {
+    super('bootGame');
+  }
+
+  preload() {
+    this.load.image("background", "assets/img/background.png");
+  }
+  create() {
+    this.add.text(20, 20, "Loading game...");
+    this.scene.start("playGame");
+  }
+}
